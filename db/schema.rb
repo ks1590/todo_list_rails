1e4bfa8e161019d12a320e9a4f2ae1a8c0625a8b
kensuke suzuki <35k.0101@gmail.com>
2020-12-19 22:27:03 +0900

    Userモデル作成

---
+++ db/schema.rb
@@ -0,0 +1,14 @@
+ActiveRecord::Schema.define(version: 2020_12_19_131412) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
