710bbb85cb9d68f43a5dc6d8ef53c9ece7322c5d
kensuke suzuki <35k.0101@gmail.com>
2020-12-20 12:52:52 +0900

    新規登録フォーム作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,16 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: "登録しました！"
+    else
+      render :new
+    end
   end
 
   def edit
@@ -11,4 +21,13 @@ class TasksController < ApplicationController
 
   def show
   end
+
+  private
+  def task_params
+    params.require(:task).permit(
+      :name,
+      :content
+    )    
+  end
+  
 end
