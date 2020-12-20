4ac79c344d932b83424388921bf562e76980f85d
kensuke suzuki <35k.0101@gmail.com>
2020-12-20 22:03:25 +0900

    詳細画面を実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:edit, :update]
+  before_action :set_task, only: [:edit, :update, :show]
 
   def index
     @tasks = Task.all
