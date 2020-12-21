a2152894cdf9ad984e5d6c190e3f5640e09d1960
kensuke suzuki <35k.0101@gmail.com>
2020-12-21 21:54:49 +0900

    Todoの項目を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_134424) do
+ActiveRecord::Schema.define(version: 2020_12_21_122932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2020_12_19_134424) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deadline"
+    t.string "status", default: "未着手", null: false
+    t.integer "priority", default: 0, null: false
   end
 
   create_table "users", force: :cascade do |t|
