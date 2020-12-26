93953652c19b758d33e84e5ff422a607342b9fec
kensuke suzuki <35k.0101@gmail.com>
2020-12-26 23:07:48 +0900

    モデルテスト作成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+    FactoryBot.create(:third_task)
+    visit tasks_path
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -11,14 +18,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         select "完了", from: 'task_status'
         click_on "登録"
         expect(page).to have_content "テスト1"
-        binding.irb
       end
     end
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        visit tasks_path
         expect(page).to have_content 'テスト1'
       end
     end
