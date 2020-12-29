ebd437e5e1e9d19e8b5b04a97e9227f0bfae8c6b
kensuke suzuki <35k.0101@gmail.com>
2020-12-29 23:51:46 +0900

    フラッシュメッセージ表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to tasks_path
     else
-      flash.now[:danger] = "ログインに失敗しました。"
+      flash[:danger] = "ログインに失敗しました。"
+      render :new
     end
   end
   
