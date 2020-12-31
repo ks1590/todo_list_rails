a1149c8e0aedb7f217f7d92c35d6f71417770ffe
kensuke suzuki <35k.0101@gmail.com>
2020-12-31 20:55:56 +0900

    エラーを日本語化

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,14 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module TodoListRails
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-    config.time_zone = 'Asia/Tokyo'
-    # config.active_record.default_timezone = :local
-    # config.i18n.default_locale = :ja
+  class Application < Rails::Application    
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     config.generators do |g|
+    config.load_defaults 5.2
+      g.assets false
+      g.helper false
       g.test_framework :rspec,
                       model_specs: true,
                       view_specs: false,
