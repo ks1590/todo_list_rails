6d648dcf5b7f172a21b33387d6d9432deeb2ecf1
kensuke suzuki <35k.0101@gmail.com>
2020-12-31 17:58:30 +0900

    既存カラムの変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_122932) do
+ActiveRecord::Schema.define(version: 2020_12_31_085555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_122932) do
     t.text "content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deadline"
+    t.datetime "deadline", null: false
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
   end
