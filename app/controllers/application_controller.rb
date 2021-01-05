9c0e50e53c4f6000ee0f10767b901db9247d45cc
kensuke suzuki <35k.0101@gmail.com>
2021-01-05 21:19:56 +0900

    Basic認証設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'test' && password == 'pass1234'
+    end
+  end
 
   def authenticate_user
     if current_user == nil
