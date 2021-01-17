1b963d53fc412c13bbf74b142db1a45bc8907b11
kensuke suzuki <35k.0101@gmail.com>
2021-01-17 19:36:56 +0900

    seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+I18n.config.available_locales = :en
+
+5.times do |n|
+  name = Faker::JapaneseMedia::Doraemon.character
+  email = "test#{ n + 1 }@example.com"
+  password = "123456"
+
+  User.create!(
+    name: name,
+    email: email,
+    password: password,
+    password_confirmation: password
+  )
+end
+
+status = ["未着手","着手中","完了"]
+start_date = Date.parse("2021/02/01")
+end_date = Date.parse("2021/02/28")
+
+20.times do |n|
+  get_date = Random.rand(start_date .. end_date)
+  title = Faker::Movies::HarryPotter.book
+  description = Faker::Movies::HarryPotter.quote
+  status = ["未着手","着手中","完了"]
+
+  Task.create!(name: title,
+              content: description,
+              deadline: get_date,
+              status: status.sample,
+              priority: rand(0..2),
+              user_id: rand(1..5))
+end
