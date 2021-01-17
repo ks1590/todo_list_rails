71ae5a7e007026f3ce7eada6890c408639552e25
kensuke suzuki <35k.0101@gmail.com>
2021-01-17 11:10:38 +0900

    フォームのエラー表示変更

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,9 @@ module TodoListRails
   class Application < Rails::Application    
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.generators do |g|
-    config.load_defaults 5.2
       g.assets false
       g.helper false
       g.test_framework :rspec,
@@ -22,6 +22,21 @@ module TodoListRails
                       routing_specs: false,
                       controller_specs: false,
                       request_specs: false
+    end
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        html_tag.html_safe
+      else
+        class_name = instance.object.class.name.underscore
+        method_name = instance.instance_variable_get(:@method_name)
+        "<div class=\"has-error\">#{html_tag}
+          <span class=\"help-block\">
+            ! #{I18n.t("activerecord.attributes.#{class_name}.#{method_name}")}           
+            #{instance.error_message.first}
+          </span>
+        </div>".html_safe
+      end
     end
   end
 end
